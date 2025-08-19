10724df25e5fd021796e128e3cb9ee3ab293bbc5
vivek1mishra <mvivek107@gmail.com>
2025-08-19 23:49:40 +0530

    Update Dhondhu.tf

--- Dhondhu.tf
+++ Dhondhu.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "26a037c9-baf8-4ed0-b3ed-bb41dc6a3ddb"
+  subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
 }
 
 resource "azurerm_resource_group" "rahul" {
@@ -17,30 +17,6 @@ resource "azurerm_resource_group" "rahul" {
   location = "West US"
 }
 
-resource "azurerm_storage_account" "rahulstg" {
-  name                     = "monkeysdf"
-  resource_group_name      = azurerm_resource_group.rahul.name
-  location                 = azurerm_resource_group.rahul.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
 
-
-
-resource "azurerm_storage_container" "rahulstgcontainer" {
-  name                  = "test"
-  storage_account_name  = azurerm_storage_account.rahulstg.name
-  container_access_type = "private"
-}
-
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rahul-rg"
-    storage_account_name = "monkeysdf"
-    container_name       = "test"
-    key                  = "terraform.tfstate"
-  }
-}
 
 
